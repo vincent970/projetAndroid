646abf0d99eca825853002bad6e53cde71672bbc
Jessy Rodrigue <rodrigue_jes@hotmail.com>
2019-04-04 14:18:20 -0400

    Ajout d'un get pour la BD

--- HungryforApples/app/src/main/java/com/exeinformatique/hungryforapples/MainActivity.java
+++ HungryforApples/app/src/main/java/com/exeinformatique/hungryforapples/MainActivity.java
@@ -1,13 +1,53 @@
 package com.exeinformatique.hungryforapples;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "DatabaseActivity";
+    FirebaseFirestore db = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        db = FirebaseFirestore.getInstance();
+
+        db.collection("Restaurants")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            StringBuilder restaurantNames = new StringBuilder();
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                if (restaurantNames.length() != 0) restaurantNames.append("\n");
+                                restaurantNames.append(document.getString("Name"));
+                            }
+                            Toast.makeText(
+                                    MainActivity.this,
+                                    restaurantNames.toString(),
+                                    Toast.LENGTH_LONG
+                            ).show();
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
     }
+
 }
