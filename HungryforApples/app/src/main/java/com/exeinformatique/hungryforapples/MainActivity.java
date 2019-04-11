dc9109d78d70dfc05cb9fd6490d8291b412c0db7
jessyjr97 <rodrigue_jes@hotmail.com>
2019-04-10 20:11:17 -0400

    Ajout du get pour le nom du restaurant et sa position

--- HungryforApples/app/src/main/java/com/exeinformatique/hungryforapples/MainActivity.java
+++ HungryforApples/app/src/main/java/com/exeinformatique/hungryforapples/MainActivity.java
@@ -8,13 +8,11 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
-
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "DatabaseActivity";
@@ -26,28 +24,47 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         db = FirebaseFirestore.getInstance();
 
+        Map<String, Map> restaurantsLocation = getAllRestaurantsPosition();
+        for (Map.Entry<String, Map> restaurant: restaurantsLocation.entrySet()) {
+            String message = restaurant.getKey() + ": " + restaurant.getValue();
+            Log.d(TAG, "showing Toast with message: " + message);
+            Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private Map<String, Map> getAllRestaurantsPosition() {
+        final Map<String, Map> restaurantsPosition = new HashMap<>();
         db.collection("Restaurants")
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
-                            StringBuilder restaurantNames = new StringBuilder();
                             for (QueryDocumentSnapshot document : task.getResult()) {
                                 Log.d(TAG, document.getId() + " => " + document.getData());
-                                if (restaurantNames.length() != 0) restaurantNames.append("\n");
-                                restaurantNames.append(document.getString("Name"));
+                                String name = document.getString("Name");
+                                Map position = getRestaurantPosition(document);
+                                if (name != null && !position.isEmpty()) {
+                                    restaurantsPosition.put(name, position);
+                                    Log.d(TAG, "added restaurant " + name
+                                            + " and it's position in Map");
+                                }
                             }
-                            Toast.makeText(
-                                    MainActivity.this,
-                                    restaurantNames.toString(),
-                                    Toast.LENGTH_LONG
-                            ).show();
-                        } else {
-                            Log.d(TAG, "Error getting documents: ", task.getException());
-                        }
+                        } else Log.d(TAG, "Error getting documents: ", task.getException());
                     }
                 });
+        return restaurantsPosition;
     }
 
+    private Map<Double, Double> getRestaurantPosition(QueryDocumentSnapshot document) {
+        Map<Double, Double> position = new HashMap<>();
+        Map location = (HashMap) document.get("Position");
+        if (location != null) {
+            Double latitude = (Double) location.get("Latitude");
+            Double longitude = (Double) location.get("Longitude");
+            if (latitude != null && longitude != null)
+                position.put(latitude, longitude);
+        }
+        return position;
+    }
 }
