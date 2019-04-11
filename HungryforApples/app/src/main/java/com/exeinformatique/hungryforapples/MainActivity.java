3b83a4ced60b2963d1e825a1d8960401b83680f0
Jessy Rodrigue <rodrigue_jes@hotmail.com>
2019-04-11 13:28:59 -0400

    Finalisation du get et ajout d'une fct pour créer les markers

--- HungryforApples/app/src/main/java/com/exeinformatique/hungryforapples/MainActivity.java
+++ HungryforApples/app/src/main/java/com/exeinformatique/hungryforapples/MainActivity.java
@@ -16,6 +16,7 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "DatabaseActivity";
+    private Map<String, Map<String,Double>> restaurantsPosition;
     FirebaseFirestore db = null;
 
     @Override
@@ -24,16 +25,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         db = FirebaseFirestore.getInstance();
 
-        Map<String, Map> restaurantsLocation = getAllRestaurantsPosition();
-        for (Map.Entry<String, Map> restaurant: restaurantsLocation.entrySet()) {
-            String message = restaurant.getKey() + ": " + restaurant.getValue();
-            Log.d(TAG, "showing Toast with message: " + message);
-            Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
-        }
+        getAllRestaurantsPosition();
     }
 
-    private Map<String, Map> getAllRestaurantsPosition() {
-        final Map<String, Map> restaurantsPosition = new HashMap<>();
+    private void getAllRestaurantsPosition() {
+        restaurantsPosition = new HashMap<>();
         db.collection("Restaurants")
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
@@ -43,27 +39,45 @@ public class MainActivity extends AppCompatActivity {
                             for (QueryDocumentSnapshot document : task.getResult()) {
                                 Log.d(TAG, document.getId() + " => " + document.getData());
                                 String name = document.getString("Name");
-                                Map position = getRestaurantPosition(document);
+                                Map<String,Double> position = getRestaurantPosition(document);
                                 if (name != null && !position.isEmpty()) {
-                                    restaurantsPosition.put(name, position);
+                                    addRestaurantPosition(name, position);
                                     Log.d(TAG, "added restaurant " + name
                                             + " and it's position in Map");
                                 }
                             }
+                            generateMarkers();
                         } else Log.d(TAG, "Error getting documents: ", task.getException());
                     }
                 });
-        return restaurantsPosition;
     }
 
-    private Map<Double, Double> getRestaurantPosition(QueryDocumentSnapshot document) {
-        Map<Double, Double> position = new HashMap<>();
+    private void addRestaurantPosition(String name, Map<String,Double> position) {
+        restaurantsPosition.put(name, position);
+    }
+
+    private void generateMarkers() {
+        for (Map.Entry<String, Map<String,Double>> restaurant: restaurantsPosition.entrySet()) {
+            String restaurantName = restaurant.getKey();
+            Map<String,Double> location = restaurant.getValue();
+            Double lat = location.get("Latitude");
+            Double lng = location.get("Longitude");
+            String message = restaurantName + ": " + lat + ',' + lng;
+            Log.d(TAG, "showing Toast with message: " + message);
+            Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private Map<String, Double> getRestaurantPosition(QueryDocumentSnapshot document) {
+        Map<String, Double> position = new HashMap<>();
         Map location = (HashMap) document.get("Position");
         if (location != null) {
             Double latitude = (Double) location.get("Latitude");
             Double longitude = (Double) location.get("Longitude");
-            if (latitude != null && longitude != null)
-                position.put(latitude, longitude);
+            if (latitude != null && longitude != null) {
+                position.put("Latitude", latitude);
+                position.put("Longitude", longitude);
+            }
         }
         return position;
     }
