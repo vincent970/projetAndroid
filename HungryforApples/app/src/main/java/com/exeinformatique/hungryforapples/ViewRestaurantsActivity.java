28c201380df2f6224b1408abcdfbd3869be23ea2
Unknown <vlarochelle970@gmail.com>
2019-04-11 07:12:15 -0400

    Demander permission pour GPS

--- HungryforApples/app/src/main/java/com/exeinformatique/hungryforapples/ViewRestaurantsActivity.java
+++ HungryforApples/app/src/main/java/com/exeinformatique/hungryforapples/ViewRestaurantsActivity.java
@@ -1,27 +1,34 @@
 package com.exeinformatique.hungryforapples;
 
+import android.Manifest;
 import android.content.Context;
+import com.google.android.gms.location.LocationServices;
+
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.security.Permission;
+
 
 public class ViewRestaurantsActivity extends FragmentActivity
         implements OnMapReadyCallback,
@@ -35,18 +42,43 @@ public class ViewRestaurantsActivity extends FragmentActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.ACCESS_FINE_LOCATION }, 0);
+        }
+        //Toast.makeText(this,"got here",Toast.LENGTH_LONG).show();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_restaurants);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-        Toast.makeText(this,"", Toast.LENGTH_LONG).show();
+        mapFragment.getMapAsync( this);
+        //Toast.makeText(this,"", Toast.LENGTH_LONG).show();
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
+//                .addOnConnectionFailedListener(this)
                 .addApi(LocationServices.API)
                 .build();
+
+       LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+       locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,0,0,locationListener);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.connect();
+        }
+    }
+
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
     }
 
     @Override
@@ -62,6 +94,7 @@ public class ViewRestaurantsActivity extends FragmentActivity
             mGoogleApiClient.disconnect();
         }
     }
+
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -71,7 +104,6 @@ public class ViewRestaurantsActivity extends FragmentActivity
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
-    @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
@@ -80,6 +112,36 @@ public class ViewRestaurantsActivity extends FragmentActivity
         mMap.addMarker(new MarkerOptions().position(sydney).title("REEEEEEEEEEEEEE"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
+    // Acquire a reference to the system Location Manager
+
+    // Define a listener that responds to location updates
+   LocationListener locationListener = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            // Called when a new location is found by the network location provider.
+            putMarkerOnMap(location, "");
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+        public void onProviderEnabled(String provider) {}
+
+        public void onProviderDisabled(String provider) {}
+    };
+
+// Register the listener with the Location Manager to receive location updates
+
+
+
+    private void putMarkerOnMap(Location location, String Tag ){
+        LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(userLocation).title(Tag));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation));
+    }
+
+
+
+
 
 
 
@@ -99,11 +161,14 @@ public class ViewRestaurantsActivity extends FragmentActivity
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
+        Location location = null;
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
 
-        //Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        Location location = new Location("");
-        location.setLatitude(25.02d);
-        location.setLongitude(32.01d);
+            String units = "imperial";
+            //new GetWeatherTask(textView).execute(url);
+        }
         if(location == null){
 
         }else{
