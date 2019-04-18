76ea7ea4538644884c20fbb3a6fdfe4002cf54f1
Vincent <a@b.cd>
2019-04-18 13:17:58 -0400

    Demander la permission au démarrage

--- HungryforApples/app/src/main/java/com/exeinformatique/hungryforapples/MainActivity.java
+++ HungryforApples/app/src/main/java/com/exeinformatique/hungryforapples/MainActivity.java
@@ -1,9 +1,14 @@
 package com.exeinformatique.hungryforapples;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,13 +17,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setListeners();
+        AskForCoarseLoactionPermission();
     }
 
     public void setListeners(){
         findViewById(R.id.btn_gotoViewRestaurants).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                gotoViewRestaurants();
+                if(checkPermissionCoarseLocation()==true){
+                    gotoViewRestaurants();
+                }
+
             }
         });
     }
@@ -26,4 +35,23 @@ public class MainActivity extends AppCompatActivity {
     private void gotoViewRestaurants(){
         startActivity(new Intent(this, ViewRestaurantsActivity.class));
     }
+
+    private boolean checkPermissionCoarseLocation(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        }else{
+            AskForCoarseLoactionPermission();
+            return  false;
+        }
+
+    }
+
+    private void AskForCoarseLoactionPermission(){
+            ActivityCompat.requestPermissions(this, new String[] {
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            }, 0);
+        //Toast.makeText(this, "Carl you motherfucking piece of shit gangbanging cocksucka", Toast.LENGTH_LONG).show();
+    }
+
 }
