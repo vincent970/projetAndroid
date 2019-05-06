a02074eb94b580ba82ac1ee4a0d5976d90d2492d
Yo <yo>
2019-05-06 09:12:46 -0400

    Ajout ouverture fenetre quand clic sur marker

--- HungryforApples/app/src/main/java/com/exeinformatique/hungryforapples/MainActivity.java
+++ HungryforApples/app/src/main/java/com/exeinformatique/hungryforapples/MainActivity.java
@@ -7,18 +7,10 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
